0f5f57a0cad569ff796b94c4f03bb3f95420ffe4
Alex <41188285+alexzvn@users.noreply.github.com>
2022-03-02 08:05:22 +0000

    update app name

--- src/main/java/dev/alexzvn/ManagerApp.java
+++ src/main/java/dev/alexzvn/ManagerApp.java
@@ -1,14 +1,15 @@
 package dev.alexzvn;
 
+import javax.swing.JFrame;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class ManagerApp extends JFrame
 {
     public static void main( String[] args )
     {
-
+        
     }
 }
